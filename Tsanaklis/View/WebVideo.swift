9ac28c59978fbc39270f39d618f3152412b51223
Angelos Staboulis <angelosstaboulis@gmail.com>
2024-03-13 20:39:59 +0200

    Commit minor improvements

---
+++ Tsanaklis/View/WebVideo.swift
@@ -0,0 +1,26 @@
+//
+//  WebVideo.swift
+//  Tsanaklis
+//
+//  Created by Angelos Staboulis on 13/3/24.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+struct WebVideo:UIViewRepresentable{
+    var urlVideo = String()
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+ 
+    func makeUIView(context: Context) -> some WKWebView {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.allowsInlineMediaPlayback = true
+        let webView = WKWebView(frame: .zero,configuration: webConfiguration)
+        if let urlMain = URL(string:urlVideo) {
+            webView.load(URLRequest(url: urlMain))
+        }
+        
+        return webView
+    }
+}
